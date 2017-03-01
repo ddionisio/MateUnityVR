7601849f984207893ef54ffe67c9d4bb4ddd834c
ddionisio <garapata@gmail.com>
2017-03-01 00:58:16 -0800

    just thinking about portability of controllers

---
+++ Scripts/Controller/ControllerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8.VR {
+    public abstract class ControllerManager : SingletonBehaviour<ControllerManager> {
+        protected override void OnInstanceDeinit() {
+            
+        }
+
+        protected override void OnInstanceInit() {
+            
+        }
+    }
+}
---
+++ Scripts/Controller/ControllerMap.cs
@@ -0,0 +1,23 @@
+﻿
+namespace M8.VR {
+    /// <summary>
+    /// This is a generalized mapping of the VR buttons and axis to allow for porting to various devices via derivatives of InputManager (e.g. InputManagerSteamVR).
+    /// </summary>
+    [System.Flags]
+    public enum ControlMap {
+        None = 0x0,
+
+
+    }
+    
+    public enum Hand {
+        None = -1,
+
+        Left,
+        Right,
+
+        Both,
+
+        Any
+    }
+}
